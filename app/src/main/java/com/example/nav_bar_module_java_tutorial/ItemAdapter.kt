f7c7b3d30abb615b136f65b838ef1e2dd1ac8d95
Boradeg <gokulborade05@gmail.com>
2024-03-23 23:28:31 +0530

    updated

---
+++ app/src/main/java/com/example/nav_bar_module_java_tutorial/ItemAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.nav_bar_module_java_tutorial
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(private val mList: List<DataModel>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.each_item, parent, false)
+        return ItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val model = mList[position]
+        holder.mTextView.text = model.itemText
+
+        // Set LinearLayoutManager with horizontal orientation
+        val layoutManager = LinearLayoutManager(holder.itemView.context, LinearLayoutManager.HORIZONTAL, false)
+        holder.nestedRecyclerView.layoutManager = layoutManager
+
+        val adapter = NestedAdapter(model.nestedList)
+        holder.nestedRecyclerView.setHasFixedSize(true)
+        holder.nestedRecyclerView.adapter = adapter
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val mTextView: TextView = itemView.findViewById(R.id.itemTv)
+        val nestedRecyclerView: RecyclerView = itemView.findViewById(R.id.child_rv)
+    }
+}
