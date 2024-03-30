05bbb8d59b6d08726953e6cf8948ff69d9f3cee6
Boradeg <gokulborade05@gmail.com>
2024-03-30 14:55:36 +0530

    All UI updated,splash screen update

---
+++ app/src/main/java/com/example/nav_bar_module_java_tutorial/SplashActivity2.kt
@@ -0,0 +1,25 @@
+package com.example.nav_bar_module_java_tutorial
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.RelativeLayout
+
+class SplashActivity2 : AppCompatActivity() {
+    private lateinit var lay:RelativeLayout
+    private val handler = Handler()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+        supportActionBar?.hide()
+        handler.postDelayed({
+            val intent = Intent(this@SplashActivity2, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+
+
+    }
+}
